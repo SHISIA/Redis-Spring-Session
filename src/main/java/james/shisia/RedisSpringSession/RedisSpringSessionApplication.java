04f7c0a2ed9566f4b1dbe8a8e67f65c563f4d20a
Jemo <jamesshisia6@gmail.com>
2023-09-26 17:33:11 +0300

    Initial commit

---
+++ src/main/java/james/shisia/RedisSpringSession/RedisSpringSessionApplication.java
@@ -0,0 +1,13 @@
+package james.shisia.RedisSpringSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/james/shisia/RedisSpringSession/RedisSpringSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package james.shisia.RedisSpringSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
